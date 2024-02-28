1ddfaeaa8c4450d2e8c3801971c938bbce7ee827
Emre Lutfi <mail@emrelutfi.com>
2024-02-29 00:17:39 +0300

    waitlist

---
+++ WebstormProjects/untitled37/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from "next/image";
+
+const notFound = () => {
+    return (
+        <div className={'flex flex-col justify-start items-center'}>
+                  </div>
+    );
+};
+
+export default notFound;
